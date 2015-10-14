b54dc7b52820bb6e5afca1a173d5273e47876278
Vincent Predoehl <vpredoehl@gmail.com>
2015-10-13 22:18:43 -0500

    Finds eligible digits for points with no given value

---
+++ Sudoku/Sudoku/Sudoku.h
@@ -0,0 +1,29 @@
+//
+//  Sudoku.h
+//  Sudoku
+//
+//  Created by Vincent on 10/13/15.
+//  Copyright (c) 2015 VJP. All rights reserved.
+//
+
+#ifndef Sudoku_Sudoku_h
+#define Sudoku_Sudoku_h
+
+#include <map>
+#include <set>
+#include <array>
+
+using std::pair;
+using std::map;
+using std::set;
+using std::array;
+
+using Point = pair<short,short>;
+using Grid = map<Point, short>;
+using EligibleDigits = map<Point, set<short>>;
+
+const array<short,9> columns = { 1,2,3,4,5,6,7,8,9 },
+	rows = columns,
+	digits = rows;
+
+#endif
--- Sudoku/Sudoku/main.cpp
+++ Sudoku/Sudoku/main.cpp
@@ -6,12 +6,12 @@
 //  Copyright (c) 2015 VJP. All rights reserved.
 //
 
+#include "Sudoku.h"
+
 #include <iostream>
-#include <map>
+#include <algorithm>
 
 using namespace std;
-
-typedef map<pair<short,short>, short> Grid;
 
 Grid givenValues =
 {
@@ -26,23 +26,73 @@ Grid givenValues =
 	{{2,9},1}, {{3,9},6}, {{4,9},7}, {{5,9},3}, {{6,9},4}, {{9,9},2}
 };
 
+	// output convenience
+ostream& operator<<(ostream& o, Point v)	{	o << "x: " << v.first << " y: " << v.second;	return o;	}
+ostream& operator<<(ostream& o, set<short> v)	{	for(auto e : v) cout << e << ", "; return o;	}
+
+
+set<short> FindDigitsForPoint(Point p)
+{
+	cout << "Finding candidates for point " << p << endl;
+	short pX = p.first, pY = p.second;
+
+	set<short> u,i;	// union of all givens on row pY
+
+	for(auto x: columns)
+		if(pX != x)
+		{
+			auto xPos = givenValues.find({x,pY});	// find value at this Point
+			if(xPos != givenValues.end())	u.insert(xPos->second);
+		}
+	for(auto y: rows)
+		if(pY != y)
+		{
+			auto yPos = givenValues.find({pX,y});
+			if(yPos != givenValues.end())	u.insert(yPos->second);
+		}
+	cout << "Found: " << u << endl << endl;
+
+	set_difference(digits.begin(), digits.end(), u.begin(), u.end(), inserter(i, i.begin()));
+
+	cout << "Eligible: " << i << endl << endl;
+
+	return i;
+}
+
+EligibleDigits FindEligibleDigits(const Grid& g)
+{
+	EligibleDigits e;
+	for(auto y: rows)	for(auto x: columns)
+		if(givenValues.find({x,y}) == givenValues.end())
+				// not a given value
+			e[{x,y}] = FindDigitsForPoint({x,y});
+	return e;
+}
+
 void PrintGrid(const Grid& g)
 {
-	for(short y=1; y<10; y++)
+	for(auto y : rows)
 		{
-		for(short x=1; x<10; x++)
+			for(auto x : columns)
 			{
-			auto v = givenValues.find(make_pair(x, y));
+				auto v = givenValues.find(make_pair(x, y));
 
-			if(v != givenValues.end())	cout << v->second << ' ';
-			else cout << "* ";
+				if(v != givenValues.end())	cout << v->second << ' ';
+				else cout << "* ";
 			}
-		cout << endl;
+			cout << endl;
 		}
+	cout << endl << endl;
 }
+
+
 
 int main(int argc, const char * argv[])
 {
 	PrintGrid(givenValues);
+
+	Grid solution = givenValues;
+	EligibleDigits cs = FindEligibleDigits(givenValues);
+
     return 0;
 }
