3c0aee683a0de8c56e0cf1daccbcae1662558c48
Vincent Predoehl <vpredoehl@gmail.com>
2017-02-27 17:22:33 -0600

    packaged_task experiment

--- Sudoku/Sudoku/packaged_task.cpp
+++ Sudoku/Sudoku/packaged_task.cpp
@@ -1,3 +1,5 @@
+//g++ packaged_task.cpp -std=c++11  -o packaged_task
+
 #include <iostream>
 #include <thread>
 #include <future>
@@ -8,6 +10,12 @@ using std::cout;
 using std::promise;
 using std::future;
 using std::exception_ptr;
+using std::packaged_task;
+using std::async;
+
+auto add(int a, int b) -> int	{	return a+b;	}
+auto throw_test(int a, int b) -> int	{	throw std::string("throwing a string");	}
+auto square(int i) -> double   {   return i*i; }
 
 void f(promise<int> px)
 {
@@ -18,16 +26,13 @@ void f(promise<int> px)
 		exception_ptr ep = std::current_exception();
 		px.set_exception(ep);
 	}
-	//px.set_exception(7.7);
 }
 
 int main()
 {
 	promise<int> p;
 	future<int> vx = p.get_future();
-	thread t(f, std::move(p));
-
-	vx.wait();
+	thread t {f, std::move(p)};
 
 	try
 	{
@@ -39,5 +44,16 @@ int main()
 		cout << "exception: " << e << std::endl;
 	}
 
+	packaged_task<int(int,int)> pt {/* add */ throw_test};
+	future<int> result = pt.get_future();
+	thread ptt { move(pt), 3, 5};
+
+	try	{	int v = result.get();	cout << "result: " << v << std::endl;	}
+	catch(std::string s)	{	cout << "string: " << s << std::endl;	}
+    
+    future<double> sqr = async(square, 4);
+    cout << "square: " << sqr.get() << std::endl;
+
 	t.join();
+	ptt.join();
 }
